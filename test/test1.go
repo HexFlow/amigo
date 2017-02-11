6c9d36e85ed6b629ed17ef2c0fa2caf4ea43c60c
Saksham Sharma <saksham0808@gmail.com>
2017-02-11 12:17:24 +0530

    Go semicolon go

---
+++ test/test1.go
@@ -0,0 +1,16 @@
+// _Channels_ are the pipes that connect concurrent
+// goroutines. You can send values into channels from one
+// goroutine and receive those values into another
+// goroutine.
+
+package main
+
+import (
+        "fmt"
+        "log"
+       )
+
+func main() {
+    a = 5 + 4/5
+    b := 4 + log(2)
+}
